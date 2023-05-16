818740713cde30342df05eaa9acbc682d69b2048
Julian Lopez <julianlopez6850@gmail.com>
2023-05-16 10:34:36 -0400

    Update GET /dates
    - Dates which are not closed, but whose Files are closed, are now categorized as closed with respect to queries where the isClosed parameter is provided.

--- routes/Dates.js
+++ routes/Dates.js
@@ -29,8 +29,8 @@ router.get("/", async (req, res) => {
 
         var datesQuery = {}
 
-        // if startDate or endDate is defined, search for dates within the range [startDate, endDate]
-        // if either value is not defined, set it to its respective limit.
+        // if startDate and/or endDate is defined, search for Dates with a date field within the
+        // range [startDate, endDate] (if one value is not defined, set it to its respective limit).
         if(startDate || endDate) {
             datesQuery.date = { [Op.between]: [startDate || '1000-01-01', endDate || '9999-12-31'] }
         }
@@ -43,11 +43,60 @@ router.get("/", async (req, res) => {
             datesQuery.isClosed = isClosed === "true" ? 1 : 0;
         }
 
-        const criticalDates = await Dates.findAll({
-            where: datesQuery
+        // get the fileNumbers of all CLOSED files (i.e. isClosed === 1)
+        const closedFiles = await Files.findAll({
+            where: { isClosed: 1 }
         });
+
+        var closedFileNumbers = []
+        for(const file of closedFiles) {
+            closedFileNumbers.push(file.dataValues.fileNumber)
+        }
+
+        var criticalDates = []
+
+        if(isClosed === 'true') {
+            // When isClosed query param = 'true', res includes Dates where all parameters are met OR 
+            // where the date's associated file is CLOSED (i.e. 'Files'.'isClosed' = 1).
+            criticalDates = await Dates.findAll({
+                where: {
+                    [Op.or]: [
+                        datesQuery,
+                        {
+                            fileNumber: {
+                                [Op.or]: closedFileNumbers
+                            }
+                        }
+                    ]
+                },
+                order: [['date', 'ASC']]
+            });
+        } else if(isClosed === 'false') {
+            // When isClosed === 'false', res includes Dates where all parameters are met AND 
+            // where the date's associated file is NOT CLOSED (i.e. 'Files'.'isClosed' = 0).
+            criticalDates = await Dates.findAll({
+                where: {
+                    [Op.and]: [
+                        datesQuery,
+                        {
+                            fileNumber: {
+                                [Op.notIn]: closedFileNumbers
+                            }
+                        }
+                    ]
+                },
+                order: [['date', 'ASC']]
+            });
+        } else {
+            // if isClosed is not defined, or it is not 'true'/'false', 
+            // res only includes Dates where all other parameters are met.
+            criticalDates = await Dates.findAll({
+                where: datesQuery,
+                order: [['date', 'ASC']]
+            })
+        }
         
-        // Get file info for each date
+        // for each date in the criticalDates array, get its respective file info and append it.
         for(var date of criticalDates) {
             const fileInfo = await Files.findOne({
                 where: { fileNumber: date.fileNumber }
@@ -55,15 +104,13 @@ router.get("/", async (req, res) => {
 
             if(fileInfo) {
                 for(const info in fileInfo.dataValues) {
-                    if(info === 'isClosed') {
+                    if(info === 'isClosed')
                         date.dataValues['isFileClosed'] = fileInfo.dataValues[info]
-                    } else {
+                    else
                         date.dataValues[`${info}`] = fileInfo.dataValues[info]
-                    }
                 }
             }
         }
-
 
         return res.status(200).json({ dates: criticalDates });
     } catch(err) {
@@ -91,7 +138,6 @@ router.put("/", async (req, res) => {
     }).catch((err) => {
         return res.status(400).json({ error: err })
     })
-
 })
 
 module.exports = router;
