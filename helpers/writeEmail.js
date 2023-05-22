43d161e2c39fb0343ba796b8dae9d773c6a9769b
Julian Lopez <julianlopez6850@gmail.com>
2023-05-22 16:31:34 -0400

    Minor UI changes to Email Table
    - Added Date & Closing Agent columns to table. (CA column just holds a placeholder for now).
    - Modified column widths, and text alignment & overflow.

--- helpers/writeEmail.js
+++ helpers/writeEmail.js
@@ -4,7 +4,7 @@ const Op = sequelize.Op
 const { getTodaysDate } = require('./getTodaysDate')
 
 const writeEmail = async (recipient) => {
-    var emailHtml = `<b style="white-space: pre-line">Good morning ${recipient.slice(0,1).toUpperCase() + (recipient.split('@'))[0].slice(1)}!<br/><br/>` +
+    var emailHtml = `<b style='white-space: pre-line'>Good morning ${recipient.slice(0,1).toUpperCase() + (recipient.split('@'))[0].slice(1)}!<br/><br/>` +
         `Below is your daily critical timeline update:<br/><br/>` +
         `<a href=${process.env.URL}>You can also view it on the web at ${process.env.URL}!</a> <--- { Not available yet }<br/><br/>`
     
@@ -53,20 +53,22 @@ const writeEmail = async (recipient) => {
     });
 
     for( const [index, group] of [isToday, isPastDue, isUpcoming].entries() ) {
-        const header = `<br/><br/><table cellspacing=0 width='767px' border='1' style="border:1px solid black">
-            <tr style='background-color:#88AACC;' width='767px'>
-                <th width='103px' align='left' style="border:1px solid black">Event</th>
-                <th width='60px' align='left' style="border:1px solid black">File No.</th>
-                <th width='600px' align='left' style="border:1px solid black">File Reference</th>
+        const header = `<br/><br/><table cellspacing=0 border='1' style='border:1px solid black; max-width='800px'>
+            <tr style='background-color:#88AACC;'>
+                <th align='left' style='border:1px solid black; width:75px;'>Date</th>
+                <th align='left' style='border:1px solid black; width:100px;'>Event</th>
+                <th align='left' style='border:1px solid black; width:55px;'>File No.</th>
+                <th align='left' style='border:1px solid black; width:400px;'>File Reference</th>
+                <th align='left' style='border:1px solid black; width:150px;'>Closing Agent</th>
             </tr>`
         if(index === 0) {
-            emailHtml += `<b style="white-space: pre-line">---------- TODAY ----------</b>${header}`
+            emailHtml += `<b style='white-space: pre-line'>---------- TODAY ----------</b>${header}`
         }
         if(index === 1) {
-            emailHtml += `<b style="white-space: pre-line">---------- PAST DUE ----------</b>${header}`
+            emailHtml += `<b style='white-space: pre-line'>---------- PAST DUE ----------</b>${header}`
         }
         if(index === 2) {
-            emailHtml += `<b style="white-space: pre-line">---------- UPCOMING ----------</b>${header}`
+            emailHtml += `<b style='white-space: pre-line'>---------- UPCOMING ----------</b>${header}`
         }
         
         if(group[0] === undefined) {
@@ -89,9 +91,11 @@ const writeEmail = async (recipient) => {
             }
 
             emailHtml += `<tr style='${(index % 2 === 0) ? 'background-color:#CCE7FF' : 'background-color:#AACCEE'};'>
-                <td valign='center' style="border:1px solid black">${date.dataValues.type === 'Escrow' ? date.dataValues.prefix : ''}${date.dataValues.type}</td>
-                <td valign='center' style="border:1px solid black">${date.dataValues.fileNumber}</td>
-                <td valign='center' style="border:1px solid black">${date.dataValues.fileRef}</td>
+                <td valign='center' style='border:1px solid black; text-align:center'>${date.dataValues.date.slice(-5)}-${date.dataValues.date.slice(2,4)}</td>
+                <td valign='center' style='border:1px solid black'>${date.dataValues.type === 'Escrow' ? date.dataValues.prefix : ''}${date.dataValues.type}</td>
+                <td valign='center' style='border:1px solid black; text-align:center'>${date.dataValues.fileNumber}</td>
+                <td valign='center' style='border:1px solid black; max-width:400px; overflow:hidden; white-space:nowrap; text-overflow: ellipsis;'>${date.dataValues.fileRef}</td>
+                <td valign='center' style='border:1px solid black; max-width:150px; overflow:hidden; white-space:nowrap; text-overflow: ellipsis;'>${'Placeholder Title, LLC'}</td>
             </tr>`
         }
         emailHtml += `</table><br/>`;
