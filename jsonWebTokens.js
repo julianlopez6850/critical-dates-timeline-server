c0adff6a816ac9bc72a72b7774a102838c93c2b9
Julian Lopez <julianlopez6850@gmail.com>
2023-07-11 12:20:00 -0400

    Fix JWT createToken and validateToken functions
    - Bcrypt hashed password is now stored in jwt payload.
    - Stored password in jwt payload is compared against the password in the database to ensure that the access token does not remain valid if the password has since been changed.
    - Console logs have been added to log:
    i) the information of the requestor, such as: the IP of the client making the request; the endpoint that is being requested; and the access token and body/query parameters that were provided.
    ii) the status of the token validation process, i.e. could the access token be verified, was the user authenticated, etc.

--- jsonWebTokens.js
+++ jsonWebTokens.js
@@ -1,10 +1,11 @@
-require('dotenv/config')
+require('dotenv/config');
+const { Users } = require('./models');
 
 const { sign, verify, decode } = require('jsonwebtoken');
 
 const createToken = (user) => {
     const accessToken = sign(
-        { username: user.username, id: user.id },
+        { username: user.username, password: user.password },
         process.env.JSON_WEB_TOKEN_SECRET,
         { expiresIn: "7d" }
     );
@@ -12,23 +13,45 @@ const createToken = (user) => {
     return accessToken;
 };
 
-const validateToken = (req, res, next) => {
-    const accessToken = req.cookies["access-token"]
+const validateToken = async (req, res, next) => {
+    console.log('\x1b[45m%s\x1b[0m', 'VALIDATING USER ACCESS TOKEN');
+    const accessToken = req.cookies["access-token"];
 
-    if(!accessToken)
-        return res.status(400).json({ error: "User not logged in." })
+    console.log('\x1b[35m%s\x1b[0m', `IP: ${req.ip}`);
+    console.log('\x1b[35m%s\x1b[0m', `Access Token: ${accessToken}`);
+    console.log('\x1b[35m%s\x1b[0m', `Endpoint: ${req.baseUrl}${req.path}`);
+    console.log('\x1b[35m%s\x1b[0m', `Body/Query Params: ${Object.keys(req.query).length === 0 ? JSON.stringify(req.body) : JSON.stringify(req.query)}`);
+
+    if(!accessToken) {
+        console.log('\x1b[31m%s\x1b[0m', 'User not logged in; Aborting request.');
+        return res.status(401).json({ error: "User not logged in." });
+    }
 
     try {
         const validToken = verify(accessToken, process.env.JSON_WEB_TOKEN_SECRET,);
 
-        if(validToken) {
+        const user = await Users.findOne({ where: { username: validToken.username } });
+
+        if(user && validToken.password === user.password) {
             req.authenticated = true;
             const decodedToken = decode(accessToken);
             req.username = decodedToken.username;
+
+            console.log('\x1b[33m%s\x1b[0m', 'User Verified.');
             return next();
         }
+
+        res.cookie('access-token', 'expired', {
+            maxAge: 1000,
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true
+        });
+        console.log('\x1b[31m%s\x1b[0m', 'ERROR: User\'s credentials have been changed; User has been logged out. Aborting request.');
+        return res.status(401).json({ error: 'User could not be authenticated. Aborting request.' });
     } catch (err) {
-        return res.status(400).json({ error: err });
+        console.log('\x1b[31m%s\x1b[0m', 'ERROR: Access token was not verified. Aborting request.');
+        return res.status(401).json({ error: 'User could not be authenticated. Aborting request.' });
     }
 }
 
