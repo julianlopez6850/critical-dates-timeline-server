17325edc219b7d0dcc2aba794c4179043f7ed835
Julian Lopez <julianlopez6850@gmail.com>
2023-07-18 16:12:38 -0400

    Updated PUT /auth/settings Endpoint
    - Verify that the provided username matches the username stored in the passed accessToken (ensure that one user cannot update the settings of another user).
    - Fix the format in which the settings are stored after being updated. (They were being stored as a JSON String, rather than a JSON Object).
    - Add comments throughout the routes\Users.js file to improve code readability.

--- routes/Users.js
+++ routes/Users.js
@@ -14,31 +14,36 @@ router.post('/login', async (req, res) => {
 
     const { username, password } = req.body;
 
+    // If {username} and/or {password} is not provided in the request body, return and alert the user/client.
     if(!(username && password)) {
         customLog.errorLog(`ERROR: Missing username and/or password. {${username}, ${password}}`);
         return res.status(400).json({ error: 'Missing username and/or password.' });
     }
 
-    const userFound = await Users.findOne({ where: { username: username } });
-
-    if (!userFound) {
+    const user = await Users.findOne({ where: { username: username } });
+    // If user does not exist, return and alert the user/client.
+    if(!user) {
         customLog.errorLog(`ERROR: Bad username and password combination. {${username}, ${password}}`);
         return res.status(400).json({ error: 'Wrong Username and Password Combination' });
     }
 
-    bcrypt.compare(password, userFound.password).then((match) => {
+    // Compare {password} with the password stored in the database.
+    bcrypt.compare(password, user.password).then((match) => {
+        // If password does not match, return and alert the user/client.
         if (!match) {
             customLog.errorLog(`ERROR: Bad username and password combination. {${username}, ${password}}`);
             return res.status(400).json({ error: 'Wrong Username and Password Combination' });
         }
 
-        const accessToken = createToken(userFound);
+        // If all checks passed...
+        // Log in the user (send an accessToken cookie to the client to keep the user logged in and authenticated).
+        const accessToken = createToken(user);
         res.cookie('access-token', accessToken, {
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
             sameSite:'none',
             secure: true
-        })
+        });
 
         customLog.successLog(`User {${username}} logged IN.`);
         res.status(200).json({ success: `User {${username.toUpperCase()}} logged IN.` });
@@ -50,19 +55,20 @@ router.post('/logout', async (req, res) => {
     customLog.endpointLog('Endpoint: POST /auth/logout');
     requestInfoLogger(req);
 
+    // Log out the user (expire their accessToken cookie).
     res.cookie('access-token', 'expired', {
         maxAge: 1000,
         httpOnly: true,
         sameSite: 'none',
         secure: true
-    })
+    });
 
     const accessToken = req.cookies["access-token"];
     const tokenPayload = verify(accessToken, process.env.JSON_WEB_TOKEN_SECRET);
     const username = tokenPayload.username;
 
-    customLog.successLog(`User ${username} logged OUT.`)
-    return res.status(200).json({ success: `User {${username.toUpperCase()}} logged OUT.` })
+    customLog.successLog(`User {${username}} logged OUT.`);
+    return res.status(200).json({ success: `User {${username.toUpperCase()}} logged OUT.` });
 });
 
 // Check if a user is logged in, and who that user is.
@@ -73,22 +79,52 @@ router.get('/profile', validateToken, async (req, res) => {
     user.settings = JSON.parse(user.settings);
 
     customLog.successLog('User profile info sent to client.');
-    res.status(200).json({ success: 'User authenticated.', username: req.username, settings: user.settings })
+    res.status(200).json({ success: 'User authenticated.', username: req.username, settings: user.settings });
 });
 
 // Update user settings.
-router.put('/settings', async (req, res) => {
+router.put('/settings', validateToken, async (req, res) => {
     const { username, settings } = req.body;
 
-    const user = await Users.findOne({ where: { username: username } });
+    // If {username} is not provided in the request body, return and alert the user/client.
+    if(!username) {
+        customLog.errorLog('ERROR: Missing required username parameter. Aborting request.');
+        return res.status(400).json({ error: 'Missing required username parameter.' });
+    }
 
+    // If {settings} is not provided in the request body, return and alert the user/client.
+    if(!settings) {
+        customLog.errorLog('ERROR: Missing required settings parameter. Aborting request.');
+        return res.status(400).json({ error: 'Missing required settings parameter.' });
+    }
+
+    const accessToken = req.cookies["access-token"];
+    const tokenPayload = verify(accessToken, process.env.JSON_WEB_TOKEN_SECRET);
+    const verifyUser = tokenPayload.username;
+
+    // If {username} does not match the username within the passed accessToken, return and log out the user.
+    if(username !== verifyUser) {
+        res.cookie('access-token', 'expired', {
+            maxAge: 1000,
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true
+        });
+
+        customLog.errorLog('ERROR: The provided username does not match the one stored within accessToken. Aborting request.');
+        return res.status(400).json({ error: 'The username provided does not match your username!' });
+    }
+
+    const user = await Users.findOne({ where: { username: username } });
+    // If user does not exist, return and alert the user/client.
     if(!user) {
-        //ERROR. User not found
-        return;
+        customLog.errorLog('ERROR: The provided username does not exist. Aborting request.');
+        return res.status(400).json({ error: 'The provided username does not exist.' });
     }
     
+    // Update the User's settings.
     Users.update(
-        { settings: JSON.stringify(settings) },
+        { settings: settings },
         { where: { username: username } }
     ).then(() => {
         customLog.successLog('Successfully updated settings.');
