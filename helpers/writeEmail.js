eb132c27b43c9dffb9f8d6e8a01079bf65d67f66
Julian Lopez <julianlopez6850@gmail.com>
2023-07-27 14:25:27 -0400

    Update the Critical Dates Table within Email
    - Fix table column widths to improve visual quality when viewed on both desktop and mobile devices (I will likely make some more changes to to improve this further).
    - Change 'Closing Agent' column to be 'Closing Agent?' and simply have a 'Yes' or 'No' for each row, indicating whether or not the company is the closing agent.

--- helpers/writeEmail.js
+++ helpers/writeEmail.js
@@ -1,7 +1,7 @@
 const { Dates, Files } = require('../models');
 const sequelize = require('sequelize');
-const Op = sequelize.Op
-const { getTodaysDate } = require('./getTodaysDate')
+const Op = sequelize.Op;
+const { getTodaysDate } = require('./getTodaysDate');
 
 const writeEmail = async (recipient) => {
     var emailHtml = `<b style='white-space: pre-line'>Good morning ${recipient.slice(0,1).toUpperCase() + (recipient.split('@'))[0].slice(1)}!<br/><br/>` +
@@ -53,13 +53,13 @@ const writeEmail = async (recipient) => {
     });
 
     for( const [index, group] of [isToday, isPastDue, isUpcoming].entries() ) {
-        const header = `<br/><br/><table cellspacing=0 border='1' style='border:1px solid black; max-width='800px'>
+        const header = `<br/><br/><table cellspacing=0 border='1' style='border:1px solid black; max-width=850px;'>
             <tr style='background-color:#88AACC;'>
-                <th align='left' style='border:1px solid black; width:75px;'>Date</th>
-                <th align='left' style='border:1px solid black; width:100px;'>Event</th>
-                <th align='left' style='border:1px solid black; width:55px;'>File No.</th>
-                <th align='left' style='border:1px solid black; width:400px;'>File Reference</th>
-                <th align='left' style='border:1px solid black; width:150px;'>Closing Agent</th>
+                <th align='left' style='border:1px solid black; width:60px; white-space:nowrap;'>Date</th>
+                <th align='left' style='border:1px solid black; width:100px; white-space:nowrap;'>Event</th>
+                <th align='left' style='border:1px solid black; width:50px; white-space:nowrap;'>File No.</th>
+                <th align='left' style='border:1px solid black; width:540px; white-space:nowrap;'>File Reference</th>
+                <th align='left' style='border:1px solid black; width:100px; white-space:nowrap;'>Closing Agent?</th>
             </tr>`
         if(index === 0) {
             emailHtml += `<b style='white-space: pre-line'>---------- TODAY ----------</b>${header}`
@@ -72,7 +72,7 @@ const writeEmail = async (recipient) => {
         }
         
         if(group[0] === undefined) {
-            emailHtml += `<tr><td colspan=5 style='background-color:#CCE7FF'><b>No Dates found for this timeframe.</b></td></tr></table><br/>`
+            emailHtml += `<tr><td colspan=5 style='background-color:#CCE7FF;'><b>No Dates found for this timeframe.</b></td></tr></table><br/>`
             continue;
         }
 
@@ -91,11 +91,11 @@ const writeEmail = async (recipient) => {
             }
 
             emailHtml += `<tr style='${(index % 2 === 0) ? 'background-color:#CCE7FF' : 'background-color:#AACCEE'};'>
-                <td valign='center' style='border:1px solid black; text-align:center'>${date.dataValues.date.slice(-5)}-${date.dataValues.date.slice(2,4)}</td>
-                <td valign='center' style='border:1px solid black'>${date.dataValues.type === 'Escrow' ? date.dataValues.prefix : ''}${date.dataValues.type}</td>
-                <td valign='center' style='border:1px solid black; text-align:center'>${date.dataValues.fileNumber}</td>
-                <td valign='center' style='border:1px solid black; max-width:400px; overflow:hidden; white-space:nowrap; text-overflow: ellipsis;'>${date.dataValues.fileRef}</td>
-                <td valign='center' style='border:1px solid black; max-width:150px; overflow:hidden; white-space:nowrap; text-overflow: ellipsis;'>${'Placeholder Title, LLC'}</td>
+                <td valign='center' style='border:1px solid black; white-space:nowrap;'>${date.dataValues.date.slice(-5)}-${date.dataValues.date.slice(2,4)}</td>
+                <td valign='center' style='border:1px solid black; white-space:nowrap;'>${date.dataValues.type === 'Escrow' ? date.dataValues.prefix : ''}${date.dataValues.type}</td>
+                <td valign='center' style='border:1px solid black; white-space:nowrap;'>${date.dataValues.fileNumber}</td>
+                <td valign='center' style='border:1px solid black; max-width: 540px; white-space:nowrap; overflow:hidden; text-overflow: ellipsis;'>${date.dataValues.fileRef}</td>
+                <td valign='center' style='border:1px solid black; max-width: 100px; white-space:nowrap;'>${JSON.parse(date.dataValues.roles).isClosingAgent ? 'Yes' : 'No'}</td>
             </tr>`
         }
         emailHtml += `</table><br/>`;
